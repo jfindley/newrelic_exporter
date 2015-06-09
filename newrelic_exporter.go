e9d5f815aa93b0c0188a795960c8cae663f82745
James Findley <jfindley@fastmail.com>
2015-06-09 01:24:55 +0100

    Added data downloading and parsing

--- newrelic_exporter.go
+++ newrelic_exporter.go
@@ -1,11 +1,25 @@
 package main
 
 import (
-// "encoding/json"
-// "net/http"
+	"bytes"
+	"crypto/tls"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+	"strings"
 
-// "github.com/prometheus/client_golang/prometheus"
+	// "github.com/prometheus/client_golang/prometheus"
 )
+
+// Header to set for API auth
+const APIHDR string = "X-Api-Key"
+
+// This is to support skipping verification for testing and
+// is deliberately not exposed to the user
+var TLSIGNORE bool = false
 
 type AppList struct {
 	Applications []struct {
@@ -36,11 +50,37 @@ type AppUsrStats struct {
 	ApdexScore   float64 `json:"apdex_score"`
 }
 
+func (a *AppList) get(api newRelicApi) error {
+
+	body, err := api.req("/v2/applications.json", "")
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(body, a)
+	return err
+
+}
+
 type MetricNames struct {
 	Metrics []struct {
 		Name   string
 		Values []string
 	}
+}
+
+func (m *MetricNames) get(api newRelicApi, appId int) error {
+
+	path := fmt.Sprintf("/v2/applications/%s/metrics.json", strconv.Itoa(appId))
+
+	body, err := api.req(path, "")
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(body, m)
+	return err
+
 }
 
 type MetricData struct {
@@ -51,6 +91,64 @@ type MetricData struct {
 			}
 		}
 	}
+}
+
+func (m *MetricData) get(api newRelicApi, appId int, values MetricNames) error {
+
+	if len(values.Metrics) != 1 {
+		return errors.New("Only one set of metric names can be processed at a time")
+	}
+
+	path := fmt.Sprintf("/v2/applications/%s/metrics/data.json", strconv.Itoa(appId))
+
+	data := fmt.Sprintf(
+		"names[]=%s&values[]=%s&summarize=true",
+		strings.Join(values.Metrics[0].Values, "&values[]="))
+
+	body, err := api.req(path, data)
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(body, m)
+	return err
+}
+
+type newRelicApi struct {
+	server string
+	apikey string
+}
+
+func (a *newRelicApi) req(path string, data string) ([]byte, error) {
+
+	req, err := http.NewRequest("GET", a.server+path, bytes.NewBuffer([]byte(data)))
+	if err != nil {
+		return nil, err
+	}
+
+	client := &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{
+				InsecureSkipVerify: TLSIGNORE,
+			},
+		},
+	}
+
+	req.Header.Set(APIHDR, a.apikey)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != 200 {
+		return nil, fmt.Errorf("Bad response code: %s", resp.Status)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	return body, err
+
 }
 
 func main() {
--- newrelic_exporter_test.go
+++ newrelic_exporter_test.go
@@ -1,83 +1,134 @@
 package main
 
 import (
-	"encoding/json"
 	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
-// Test the data structures parse sample JSON input correctly
-func TestJsonParsing(t *testing.T) {
-	appList, err := ioutil.ReadFile("_testing/application_list.json")
+var testApiKey string = "205071e37e95bdaa327c62ccd3201da9289ccd17"
+var testApiAppId int = 9045822
+
+func TestAppListGet(t *testing.T) {
+
+	ts, err := testServer()
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	data, err := ioutil.ReadFile("_testing/metric_data.json")
+	defer ts.Close()
+
+	var api newRelicApi
+
+	api.server = ts.URL
+	api.apikey = testApiKey
+
+	var app AppList
+
+	err = app.get(api)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	names, err := ioutil.ReadFile("_testing/metric_names.json")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	var (
-		parsedApp   AppList
-		parsedNames MetricNames
-		parsedData  MetricData
-	)
-
-	err = json.Unmarshal(appList, &parsedApp)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	err = json.Unmarshal(data, &parsedData)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	err = json.Unmarshal(names, &parsedNames)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if len(parsedApp.Applications) != 1 {
+	if len(app.Applications) != 1 {
 		t.Fatal("Expected 1 application")
 	}
 
-	app := parsedApp.Applications[0]
+	a := app.Applications[0]
 
 	switch {
 
-	case app.Id != 9045822:
+	case a.Id != testApiAppId:
 		t.Fatal("Wrong ID")
 
-	case app.Health != "green":
+	case a.Health != "green":
 		t.Fatal("Wrong health status")
 
-	case app.Name != "Test/Client/Name":
+	case a.Name != "Test/Client/Name":
 		t.Fatal("Wrong name")
 
-	case app.AppSummary.Throughput != 54.7:
+	case a.AppSummary.Throughput != 54.7:
 		t.Fatal("Wrong throughput")
 
-	case app.UsrSummary.ResponseTime != 4.61:
+	case a.UsrSummary.ResponseTime != 4.61:
 		t.Fatal("Wrong response time")
 
 	}
 
-	if len(parsedData.Metric_Data.Metrics) != 1 {
+}
+
+func TestMetricNamesGet(t *testing.T) {
+
+	ts, err := testServer()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer ts.Close()
+
+	var api newRelicApi
+
+	api.server = ts.URL
+	api.apikey = testApiKey
+
+	var names MetricNames
+
+	err = names.get(api, testApiAppId)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(names.Metrics) != 1 {
+		t.Fatal("Expected one name set")
+	}
+
+	if len(names.Metrics[0].Values) != 10 {
+		t.Fatal("Expected 10 metric names")
+	}
+
+	if names.Metrics[0].Name != "Datastore/statement/JDBC/messages/insert" {
+		t.Fatal("Wrong application name")
+	}
+
+}
+
+func TestMetricValuesGet(t *testing.T) {
+
+	ts, err := testServer()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer ts.Close()
+
+	var api newRelicApi
+
+	api.server = ts.URL
+	api.apikey = testApiKey
+
+	var values MetricData
+	var names MetricNames
+
+	err = names.get(api, testApiAppId)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = values.get(api, testApiAppId, names)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(values.Metric_Data.Metrics) != 1 {
 		t.Fatal("Expected 1 metric set")
 	}
 
-	if len(parsedData.Metric_Data.Metrics[0].Timeslices) != 1 {
+	if len(values.Metric_Data.Metrics[0].Timeslices) != 1 {
 		t.Fatal("Expected 1 timeslice")
 	}
 
-	appData := parsedData.Metric_Data.Metrics[0].Timeslices[0]
+	appData := values.Metric_Data.Metrics[0].Timeslices[0]
 
 	if len(appData.Values) != 10 {
 		t.Fatal("Expected 10 data points")
@@ -90,4 +141,51 @@ func TestJsonParsing(t *testing.T) {
 	if appData.Values["calls_per_minute"] != 2.03 {
 		t.Fatal("Wrong calls_per_minute value")
 	}
+
+}
+
+func testServer() (ts *httptest.Server, err error) {
+	TLSIGNORE = true
+
+	ts = httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		if r.Header.Get(APIHDR) != testApiKey {
+			w.WriteHeader(403)
+		}
+
+		var body []byte
+
+		switch r.URL.Path {
+
+		case "/v2/applications.json":
+			body, err = ioutil.ReadFile("_testing/application_list.json")
+			if err != nil {
+				return
+			}
+
+		case "/v2/applications/9045822/metrics.json":
+			body, err = ioutil.ReadFile("_testing/metric_names.json")
+			if err != nil {
+				return
+			}
+
+		case "/v2/applications/9045822/metrics/data.json":
+			body, err = ioutil.ReadFile("_testing/metric_data.json")
+			if err != nil {
+				return
+			}
+
+		default:
+			println(r.URL.Path)
+			w.WriteHeader(404)
+			return
+
+		}
+
+		w.WriteHeader(200)
+		w.Write(body)
+
+	}))
+
+	return
 }
